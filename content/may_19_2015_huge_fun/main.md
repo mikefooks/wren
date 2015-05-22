77b625dc779b30aa7458499dc8b610ca1475b876
Mike Fooks <mikefooks@gmail.com>
2015-05-22 17:27:22 -0400

    some refactoring

--- content/may_19_2015_huge_fun/main.md
+++ content/may_19_2015_huge_fun/main.md
@@ -4,7 +4,7 @@ Or, at least, that's how I hope this is going to work out.
 I'm sort of amazed at how quickly I can type these days; it would seem that all this practice has paid off.
 A couple of year ago I made a decision to stop looking at the keyboard as I type.
 It was extremely difficult at first.
-Nobody had ever taught me how to type properly, and so I simply poked at the keyboard at fast as I could with my index and middle fingers.
+Nobody had ever taught me how to type properly, and so I simply poked at the keyboard at fast as I could with my index and middle fingers---not a good way to type.
 
 +Incredibly interesting bullet points
 +Another wonderful bullet point.
--- generate.js
+++ generate.js
@@ -6,31 +6,44 @@ var fs = require("fs"),
   marked = require("marked"),
   _ = require("lodash");
 
-var contentDir = path.join(process.cwd(), "content");
+var mainContentDir = path.join(process.cwd(), "content");
+
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  gfm: true
+});
+
+function prependSubDir (dirName) {
+  return _.partial(path.join, mainContentDir, dirName);
+}
+
+function parseFrontmatter (path) {
+  var frontMatter = fs.readFileSync(path, { encoding: "utf8" });
+  return JSON.parse(frontMatter);
+}
+
+function compileMarkdown (path) {
+  var rawMarkdown = fs.readFileSync(path, { encoding: "utf8" });
+  return marked(rawMarkdown);
+}
 
 async.waterfall([
   // Reads the main content directory, and passes along the names
   // of the content subfolders (articles).
   function (cb) {
-    fs.readdir(contentDir, function (err, stat) {
+    fs.readdir(mainContentDir, function (err, stat) {
         cb(null, stat);
     });
   },
-  // Takes the name of each article directory and creates a 
-  // collection of objects with full paths to the markdown
-  // and frontmatter.json files.
   function (dirs, cb) {
-    cb(null, _.map(articleDirs, function (dir) {
-      var dirName = path.join(contentDir, dir);
+    async.mapLimit(dirs, 5, function (dir, cb) {
+      var prepend = prependSubDir(dir);
 
-      return {
-        frontMatter: path.join(dirName, "frontmatter.json"),
-        main: path.join(dirName, "main.md")
-      };
-    }));
-  },
-  function (content, cb) {
-    var 
+      cb({
+        frontmatter: parseFrontmatter(prepend("frontmatter.json")),
+        main: compileMarkdown(prepend("main.md"))
+      });
+    }, cb);
   }], function (err, result) {
     if (err) { return console.log(err); }
 
