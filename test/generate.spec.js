e96d5e904c7f481c97ff262b0b01830c899e648a
Mike Fooks <mikefooks@gmail.com>
2016-07-03 15:04:24 -0400

    going to rethink tests. need to work out default theme

--- test/generate.spec.js
+++ test/generate.spec.js
@@ -1,42 +1,13 @@
 "use strict";
 
 let assert = require("chai").assert,
+  _ = require("lodash"),
   qfs = require("q-io/fs"),
   path = require("path"),
   compile = require("../lib/compile.js").compilePostCollection,
   generate = require("../lib/generate.js"),
   config = {
-    contentDir: "fixtures/content",
-    themeDir: "fixtures/theme",
-    publicDir: "fixtures/public"
+    contentDir: path.join(__dirname, "fixtures/content"),
+    themeDir: path.join(__dirname, "fixtures/theme"),
+    publicDir: path.join(__dirname, "fixtures/public")
   };
-
-describe("#__generateIndex()", function () {
-  let site,
-    target;
-
-  beforeEach(function () {
-    site = compile(config)
-      .then(function (site) {
-        target = site.posts[0].target;
-      });
-  });
-
-  it("post directory is created", function () {
-    site
-      .then(function (generated) {
-        return qfs.stat(target)
-          .tap(function (stat) {
-            console.log(stat.isDirectory());
-          })
-          .then(function (stat) {
-            assert.equal(true, false);
-          });
-      });
-  });
-
-  afterEach(function () {
-    site = undefined;
-    target = undefined;
-  });
-});
