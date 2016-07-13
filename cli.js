b3659c25cbbf11ab9cffeaa6673e8747b5c75300
Mike Fooks <mikefooks@gmail.com>
2016-07-13 10:41:47 -0700

    cleaned up cli.js

--- cli.js
+++ cli.js
@@ -1,13 +1,9 @@
 "use strict";
 
 let path = require("path"),
-  _ = require("lodash"),
-  Q = require("q"),
-  qfs = require("q-io/fs"),
   program = require("commander"),
-  uuid = require("node-uuid"),
-  G = require("./lib/generate.js"),
-  U = require("./lib/utilities.js"),
+  generate = require("./lib/generate.js").generateAll,
+  create = require("./lib/create.js").createNewPost,
   config = require("./config.js");
 
 program
@@ -16,12 +12,12 @@ program
 program
   .command("generate")
   .description("generates the site")
-  .action(G.generateAll);
+  .action(generate);
 
 program
   .command("new")
   .description("Creates a new post")
   .option("-t, --title <s>", "Adds a title to the post")
-  .action(options => createNewPost(options.title));
+  .action(options => create(config, options.title));
 
 program.parse(process.argv);
